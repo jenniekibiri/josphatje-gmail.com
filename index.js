cb748b0b46d86ef3d91263e07e850f80fa971a52
jenniekibiri <josphatje@gmail.com>
2020-04-02 13:07:22 +0300

    init

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const { pool } = require('./config')
 
 const app = express()
-
+app.use(helmet())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
